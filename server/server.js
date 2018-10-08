98226419219b78dc4b0d5afa9c0f63cd36284106
cata <catalin.rscata@gmail.com>
2018-10-08 13:36:52 +0300

    add patch /todo/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -67,6 +68,33 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'copleted']);
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    if(_.isBoolean(body.copleted) && body.copleted) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.copleted = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
 });
 
 
