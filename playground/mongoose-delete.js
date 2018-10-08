f561acab805aa58633626123b1c350a073847269
cata <catalin.rscata@gmail.com>
2018-10-08 11:29:04 +0300

    add DELETE \todos\:id route

---
+++ playground/mongoose-delete.js
@@ -0,0 +1,11 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+Todo.remove({}).then((result) => {
+    console.log(result);
+});
+
+// Todo.findOneAndRemove()
+
+// Todo.findByIdAndRemove()
