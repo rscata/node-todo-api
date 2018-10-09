fbcd7728890e34e022fe7f45f6f9c844726403b6
cata <catalin.rscata@gmail.com>
2018-10-09 14:07:05 +0300

    hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var passwoed = '123abc!';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(passwoed, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassowrd = '$2a$10$ulNlVicakLnAccOCLseo6OMpPqz3NwdwrldNicHwGaRLoFzRRHtlS';
+bcrypt.compare(passwoed, hashedPassowrd, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
 
-var decode = jwt.verify(token, '123abc');
-console.log('decode', decode);
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decode = jwt.verify(token, '123abc');
+// console.log('decode', decode);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
