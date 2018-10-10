2b6fd4e110293271ea29b303e48d52d99f939353
cata <catalin.rscata@gmail.com>
2018-10-10 10:26:57 +0300

    logout

--- server/models/user.js
+++ server/models/user.js
@@ -93,6 +93,17 @@ UserSchema.statics.findByCredentials = function (email, password) {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    });
+}
 
 UserSchema.pre('save', function (next) {
     var user = this;
